db65e803aa156a1424d061fce1bcbae0c8efae33
Erik Krieg <erik.c.krieg@gmail.com>
2016-12-18 12:40:08 -0500

    installs gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var util = require('gulp-util');
+
+gulp.task('default', function () {
+    console.log('hello world', arguments);
+});
