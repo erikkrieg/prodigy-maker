8fa712388390872ef5753cf14d3e7d07ff281d63
Erik Krieg <erik.c.krieg@gmail.com>
2016-12-24 13:57:13 -0500

    adds build task for scss

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,9 @@ var PATHS = {
         'node_modules/lodash/lodash.js',
         'node_modules/phaser/build/phaser.js'
     ],
-    SCSS: []
+    SCSS: [
+        'src/scss/**/*.scss'
+    ]
 };
 
 var config = {
@@ -47,3 +49,11 @@ gulp.task('js-app', function () {
         .pipe(gulpif(config.uglify, uglify()))
         .pipe(gulp.dest('dist'));
 });
+
+gulp.task('scss', function () {
+    return gulp.src(PATHS.SCSS)
+        .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
+        .pipe(sass(config.sass))
+        .pipe(gulpif(config.sourceMaps, sourcemaps.write('.')))
+        .pipe(gulp.dest('dist'))
+})
