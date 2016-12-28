68dd0d6560018da883d6198072c750f0f5f9d17d
Erik Krieg <erik.c.krieg@gmail.com>
2016-12-28 12:59:39 -0500

    removes resize event and prevent multiple workspace injects

--- src/js/workspace/Workspace.js
+++ src/js/workspace/Workspace.js
@@ -10,6 +10,32 @@ function Workspace(options) {
 }
 
 Workspace.prototype.inject = function inject(parentEl) {
-    parentEl.appendChild(this.el);
-    this._workspace = Blockly.inject(this.el, { toolbox: this._toolbox });
+    if (!this._workspace) {
+        parentEl.appendChild(this.el);
+        this._parentEl = parentEl;
+        this._workspace = Blockly.inject(this.el, { toolbox: this._toolbox });
+        
+        // Resizing does not seem needed, but leaving it just in case
+        // window.addEventListener('resize', this.onResize.bind(this), false);
+        // this.onResize();
+        Blockly.svgResize(this._workspace);
+    }
+};
+
+Workspace.prototype.onResize = function onResize(e) {
+    // Compute the absolute coordinates and dimensions of workspace's parent.
+    var el = this.el;
+    var parent = this._parentEl;
+    var x = 0;
+    var y = 0;
+    do {
+      x += parent.offsetLeft;
+      y += parent.offsetTop;
+      parent = parent.offsetParent;
+    } while (parent);
+    // Position blocklyDiv over blocklyArea.
+    el.style.left = x + 'px';
+    el.style.top = y + 'px';
+    el.style.width = this._parentEl.offsetWidth + 'px';
+    el.style.height = this._parentEl.offsetHeight + 'px';
 };
