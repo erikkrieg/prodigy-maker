b483e72e054f8d16d3cf85a4ccdcfb8d4c0485ea
Erik Krieg <erik.c.krieg@gmail.com>
2016-12-29 14:01:06 -0500

    adds victory modal content

--- src/js/game-modal/GameModal.js
+++ src/js/game-modal/GameModal.js
@@ -13,7 +13,14 @@
         return modal;
     }
 
+    function onClick(event) {
+        if (event.target && event.target.className.indexOf('js-modal-close') > -1) {
+            this.close();
+        }
+    }
+
     function GameModal(options) {
+        this.onClick = onClick.bind(this);
         this.render(options.content);
     }
 
@@ -21,12 +28,15 @@
         this._modal = createModal(content);
         this._overlay = createOverlay();
         this._overlay.appendChild(this._modal);
+        this._modal.addEventListener('click', this.onClick);
         document.body.appendChild(this._overlay);
     };
+    
 
     GameModal.prototype.close = function close() {
         if (this._overlay) {
             document.body.removeChild(this._overlay);
+            this._modal.removeEventListener('click', this.onClick);
             this._modal = null;
             this._overlay = null;
         }
