f228e27bb06564905bbfbfd15624cda34860fd37
Erik Krieg <erik.c.krieg@gmail.com>
2016-12-28 20:31:41 -0500

    updates how game is reset

--- src/js/game/main.js
+++ src/js/game/main.js
@@ -21,7 +21,7 @@ Game.prototype.processActions = function  processActions(actions) {
 };
 
 Game.prototype.resetState = function resetState() {
-    this._game.state.start(this._game.state.current);
+    this._game.state.restart();
 };
 
 Game.fn = function () {
